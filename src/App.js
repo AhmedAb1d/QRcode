3f56c18a875aff9b055e4565c6b4fe62664c3569
Ahmed Abid <92954851+AhmedAb1d@users.noreply.github.com>
2022-08-14 01:26:33 +0100

    1st part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import QRCode from 'react-qr-code';
 
 function App() {
+  const [firstname,setFirstname]=useState("");
+  const [lastname,setLastname]=useState(""); 
+  const [component,setComponent]=useState()
+  const handleChange1 = e=>setFirstname(e.target.value);
+  const handleChange2= e=>setLastname(e.target.value);
+  const generate= ()=>{
+    if (firstname.length<1 || lastname.length<1) 
+      setComponent(<div>Don't leave the fields empty</div>)
+    else
+    setComponent(<QRCode
+      level="Q"
+      style={{ width: 256 }}
+      value={JSON.stringify({
+        firstname:{firstname},
+        lastname:{lastname}
+      })}
+    />)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form">
+      <input className="input" placeholder="Your Firstname" onChange={handleChange1}/><br></br>
+      <input className="input" placeholder="Your Lastname" onChange={handleChange2}/><br></br>
+      <button className="button" onClick={generate}>Generate QR code</button><br></br>
+      {component}
     </div>
   );
 }
